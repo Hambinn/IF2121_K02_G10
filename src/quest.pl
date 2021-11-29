c03cbe02e9d2cdc11c9d2dd2ef08581bf3a4295e
Hambinn <ilhambintangn@gmail.com>
2021-11-29 09:26:55 +0700

    benerin write quest

--- src/quest.pl
+++ src/quest.pl
@@ -3,7 +3,8 @@ quest:-
     positionX(X),
     positionY(Y),
     isQuest(X,Y),
-    write('Temukan 3 Tuna, 5 Potato, dan 3 large egg untuk mendapatkan 1000 player xp dan 5000 gold'),nl,
+    
+    write('Temukan 3 Tuna, 5 Potato, dan 3 large egg untuk mendapatkan 2000 player xp dan 7000 gold'),nl,
     write('Silahkan kembali lagi jika kamu telah memiliki semua item diatas untuk menukarnya'),nl,
     amountItem(tuna, A),
     amountItem(potato, B),
@@ -14,13 +15,13 @@ quest:-
         Pilihan == 'y' ->
         (
             3 =< A, 5=< B, 3 =< C ->
-            addGeneralExp(1000),
-            addMoney(5000),
+            addGeneralExp(2000),
+            addMoney(7000),
             removeItems(tuna, 3),
             removeItems(potato, 5),
             removeItems(large_egg, 3),
             write('Selamat kamu menyelesaikan quest!'),nl,
-            (write('Kamu mendapatkan 1000 player xp dan 5000 gold'),nl);
+            (write('Kamu mendapatkan 2000 player xp dan 7000 gold'),nl);
             (write('Item mu belum cukup! Coba kembali lain waktu!'),nl)
         );
         (
